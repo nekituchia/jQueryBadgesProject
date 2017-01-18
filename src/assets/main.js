41067ccaf4fc69c9d2b3be2081f775e54b501254
nekituchia <nikita-ushenko@mail.ru>
2017-01-18 13:20:55 +0200

    First

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/Nikusi4.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      addCourses(response.courses.completed);
+    }
+  });
+    function addCourses(courses){
+    var $badges = $('#badges');
+    courses.forEach(function(course){
+        $('<div />',{
+            'class': 'course'
+            }).appendTo($badges);
+        })
+    }
 });
